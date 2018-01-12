fe5fcae980e31d3ca667e8471fc798ae48567b8b
johnincampo <jincampo28@gmail.com>
2018-01-12 09:38:28 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// on scroll go to the next section //
+
+/* if section is larger than viewport then 
+disable scroll function until you reach the bottom of the section
+
+/*Ease in and Out when scrolling*/
+
+
